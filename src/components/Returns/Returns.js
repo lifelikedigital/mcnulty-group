d25f5588ecbd1f2acee24b8d8306f3c7dd8cf805
georgezikos <george.zikos@icloud.com>
2022-09-02 14:18:21 -0400

    Getting to the bottom of this stray label

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -23,7 +23,7 @@ Chart.defaults.set('plugins.datalabels', {
   anchor: 'end',
   align: 'top',
   clamp: true,
-  offset: 3,
+  offset: -10, //3
   formatter: function (value) {
     return `$${value}K`;
   },
