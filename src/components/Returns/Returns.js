99331de602cbc43519ccd937836c2887d76affcb
georgezikos <george.zikos@icloud.com>
2022-09-02 15:58:49 -0400

    Rusty

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -25,7 +25,7 @@ Chart.defaults.set('plugins.datalabels', {
   // clamp: true,
   // offset: 3,
   formatter: function (value) {
-    console.log(!undefined ? value : null); //`$${value}K`;
+    console.log(value === !undefined ? value : null); //`$${value}K`;
   },
 });
 
