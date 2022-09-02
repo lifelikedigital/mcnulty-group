cdc137342d7a905a3ae69b36e4a9407761246c3b
georgezikos <george.zikos@icloud.com>
2022-09-02 15:49:00 -0400

    So weird

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -25,7 +25,7 @@ Chart.defaults.set('plugins.datalabels', {
   // clamp: true,
   // offset: 3,
   formatter: function (value) {
-    return value; //`$${value}K`;
+    return value + ' K'; //`$${value}K`;
   },
 });
 
