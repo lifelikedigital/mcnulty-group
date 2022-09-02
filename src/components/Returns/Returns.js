f1d411fa30478a218429fcaafd19edcb3c291a84
georgezikos <george.zikos@icloud.com>
2022-09-02 02:46:07 -0400

    First attempt at styling and configuring chart

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -1,24 +1,18 @@
 import Chart from 'chart.js/auto';
 
 const ctx = $('#mcnulty-returns');
+const brandBlueDark = '#B9C9D4';
 
 const Returns = () => {
   const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
       datasets: [
         {
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
+          label: 'Capital Appreciation',
+          data: [190, 207.1, 227.9, 255.9, 270.5, 299.3, 336.1, 357.9, 390.5],
+          backgroundColor: [brandBlueDark],
           borderColor: [
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
@@ -26,6 +20,9 @@ const Returns = () => {
             'rgba(75, 192, 192, 1)',
             'rgba(153, 102, 255, 1)',
             'rgba(255, 159, 64, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
           ],
           borderWidth: 1,
         },
@@ -39,7 +36,6 @@ const Returns = () => {
       },
     },
   });
-  console.log('working');
 };
 
 export default Returns;
