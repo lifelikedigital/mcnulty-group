db18bbe3676d9777e227bc402f68fe3639a39ccb
georgezikos <george.zikos@icloud.com>
2022-09-02 06:39:39 -0400

    Splitting hairs

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -21,7 +21,6 @@ const Returns = () => {
           label: '',
           data: [
             ,
-            ,
             190,
             207.1,
             227.9,
@@ -33,11 +32,12 @@ const Returns = () => {
             390.5,
             ,
             ,
+            ,
           ],
           backgroundColor: [brandBlueDark],
         },
       ],
-      labels: [, , 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, ,],
+      labels: [, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, , ,],
     },
     options: {
       scales: {
