be223198ce24240b448aa586cc455b0a86e30822
georgezikos <george.zikos@icloud.com>
2022-09-02 17:18:58 -0400

    Adjusting for small viewports

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -97,6 +97,60 @@ const Returns = () => {
       },
     });
   } else if (window.matchMedia(smallScreens).matches) {
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        datasets: [
+          {
+            // prettier-ignore
+            data: [,190,207.1,227.9,255.9,270.5,299.3,336.1,357.9,390.5,],
+            backgroundColor: [brandBlueDark],
+          },
+        ],
+        // prettier-ignore
+        labels: [,2013,2014,2015,2016,2017,2018,2019,2020,2021,],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            grid: {
+              borderColor: black100,
+              color: black20,
+            },
+            ticks: {
+              callback: function (value) {
+                return `$${value}K`;
+              },
+            },
+          },
+          x: {
+            grid: {
+              borderColor: black100,
+              display: false,
+            },
+          },
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        maxBarThickness: 58,
+        barPercentage: 1,
+        devicePixelRatio: 3,
+        layout: {
+          padding: {
+            top: 20,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            enabled: false,
+          },
+        },
+      },
+    });
   } else {
     const myChart = new Chart(ctx, {
       type: 'bar',
