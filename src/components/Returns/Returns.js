1398578d30d88cd6784234653585d4b7088ab0a4
georgezikos <george.zikos@icloud.com>
2022-09-02 20:38:07 -0400

    .

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -82,8 +82,9 @@ const Returns = () => {
       },
       animations: {
         color: {
-          to: brandBlueDark,
+          duration: 3000,
           from: brandBlueLight,
+          to: brandBlueDark,
         },
       },
       maintainAspectRatio: false,
