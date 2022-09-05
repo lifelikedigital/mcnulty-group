ac30eb0b6d69d05f17ee8bff18cd9879607b9766
georgezikos <george.zikos@icloud.com>
2022-09-05 16:23:28 -0400

    Crude share links menu functionality

--- src/components/SocialShare/SocialShare.js
+++ src/components/SocialShare/SocialShare.js
@@ -1,12 +1,12 @@
 const $shareMenuLaunch = $('#share-launch'); // 'share this page' button
-const $activeShareMenu = '.share-links--active'; // display block class
+const activeShareMenu = '.share-links--active'; // display block class
 const $shareMenu = $('.share-links'); // what needs to be shown
 
 // functions
 const shareThisPage = () => {
   $shareMenuLaunch.on('click', (e) => {
     e.preventDefault();
-    console.log($shareMenuLaunch);
+    $shareMenu.toggleClass(activeShareMenu);
   });
 };
 
