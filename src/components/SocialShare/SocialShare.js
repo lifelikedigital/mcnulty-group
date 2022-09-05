4ba2336dc1d45786bc83387a6683f9dc3002f9fc
georgezikos <george.zikos@icloud.com>
2022-09-05 16:16:32 -0400

    First pass of the page sharing functionality

--- src/components/SocialShare/SocialShare.js
+++ src/components/SocialShare/SocialShare.js
@@ -1,3 +1,17 @@
-const SocialShare = () => {};
+const $shareMenuLaunch = $('#share-launch'); // 'share this page' button
+const $activeShareMenu = '.share-links--active'; // display block class
+const $shareMenu = $('.share-links'); // what needs to be shown
+
+// functions
+const shareThisPage = () => {
+  $shareMenuLaunch.on('click', (e) => {
+    e.preventDefault();
+    console.log(shareMenuLaunch);
+  });
+};
+
+const SocialShare = () => {
+  shareThisPage();
+};
 
 export default SocialShare;
