57c3f320ae63a655bf2520b093f351bef9be53e6
georgezikos <george.zikos@icloud.com>
2022-09-05 17:10:37 -0400

    Functionality for mobile viewports

--- src/components/SocialShare/SocialShare.js
+++ src/components/SocialShare/SocialShare.js
@@ -1,12 +1,22 @@
 const $shareMenuLaunch = $('#share-launch'); // 'share this page' button
 const activeShareMenu = 'share-links--active'; // display block class
 const $shareMenu = $('.share-links'); // what needs to be shown
+const $getStarted = $('#get-started'); // get started btn
+
+const activeGetStarted = 'start--active'; // toggle for get started btn
+
+const activeSharePage = 'share--active'; // toggle for share page btn
+
+// start--active
+// share--active
 
 // functions
 const shareThisPage = () => {
   $shareMenuLaunch.on('click', (e) => {
     e.preventDefault();
     $shareMenu.toggleClass(activeShareMenu);
+    $shareMenuLaunch.toggleClass(activeShareMenu);
+    $getStarted.toggleClass(activeGetStarted);
   });
 };
 
