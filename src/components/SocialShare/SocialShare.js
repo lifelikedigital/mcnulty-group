16f94dd8d1b3e60fa96436f989a1f2459a8ebb9c
georgezikos <george.zikos@icloud.com>
2022-09-05 20:16:13 -0400

    First push of social sharing link functionality

--- src/components/SocialShare/SocialShare.js
+++ src/components/SocialShare/SocialShare.js
@@ -1,3 +1,42 @@
+// Global
+const $body = $('body');
+const $window = $(window);
+// const $metaDesc = $('meta[name=description]');
+
+// URL
+const $currentPage = $(location).attr('href'); // for copying and emails
+const $currentPageEncoded = encodeURIComponent($currentPage); // for share links
+
+// Metadata
+const $currentPageTitle = $('h1').html();
+const $currentPageTitleEncoded = encodeURIComponent($currentPageTitle);
+// const $currentPageSummary = $metaDesc.attr('content');
+const $currentPageSummary = $('#summary').html();
+const $currentPageSummaryEncoded = encodeURIComponent($currentPageSummary);
+
+// Emails
+// const emailSubject = ` ${$currentPageTitle}`;
+// const emailBody = `Hey, I wanted to share this article with you on ${$currentPageTitle} from __________. You can check it out right here: ${$currentPage} – Let me know what you think!`;
+
+// Share buttons
+// const $shareBtnsContainer = $('.blog-post__share-btns');
+const $facebookShareBtn = $('#facebook-share');
+const $linkedInShareBtn = $('#linkedin-share');
+// const $emailShareBtn = $('.blog-post__share-link--email');
+const $copyShareBtn = $('#copy-link');
+const copyShareBtn = document.querySelector('#copy-link');
+
+// Share links
+const facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${$currentPageEncoded}`;
+const linkedInShareLink = `https://www.linkedin.com/shareArticle?mini=true&url=${$currentPageEncoded}&title=${$currentPageTitleEncoded}&source=https%3A%2F%2Fcanvascannabis.ca%2F&summary=${$currentPageSummaryEncoded}`;
+// const emailShareLink = `mailto:?subject=${emailSubject}&body=${emailBody}`; // gmail on ios is tricky with mailto in general + clean up body variable and subject
+
+// Pop-up window related
+const windowWidth = $window.width();
+const windowHeight = $window.height();
+const windowLeft = windowWidth / 2 - 480 / 2;
+const windowTop = windowHeight / 2 - 608 / 2;
+
 const $shareMenuLaunch = $('#share-launch'); // 'share this page' button
 const activeShareMenu = 'share-links--active'; // display block class
 const $shareMenu = $('.share-links'); // what needs to be shown
@@ -11,7 +50,7 @@ const activeSharePage = 'share--active'; // toggle for share page btn
 // share--active
 
 // functions
-const shareThisPage = () => {
+const startSharing = () => {
   $shareMenuLaunch.on('click', (e) => {
     e.preventDefault();
     $shareMenu.toggleClass(activeShareMenu);
@@ -20,8 +59,42 @@ const shareThisPage = () => {
   });
 };
 
+const copyLink = () => {
+  $copyShareBtn.on('click', (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText($currentPage);
+  });
+};
+
+const facebookShare = () => {
+  $facebookShareBtn.on('click', (e) => {
+    e.preventDefault();
+    window.open(
+      facebookShareLink,
+      'popup',
+      `menubar=no, toolbar=no, resizable=yes, scrollbars=yes, width=480, height=608, top=${windowTop}, left=${windowLeft}`
+    );
+  });
+};
+
+const linkedInShare = () => {
+  $linkedInShareBtn.on('click', (e) => {
+    e.preventDefault();
+    window.open(
+      linkedInShareLink,
+      'popup',
+      `menubar=no, toolbar=no, resizable=yes, scrollbars=yes, width=480, height=608, top=${windowTop}, left=${windowLeft}`
+    );
+  });
+};
+
 const SocialShare = () => {
-  shareThisPage();
+  startSharing();
+  copyLink();
+  $facebookShareBtn.attr('href', facebookShareLink);
+  facebookShare();
+  $linkedInShareBtn.attr('href', linkedInShareLink);
+  linkedInShare();
 };
 
 export default SocialShare;
