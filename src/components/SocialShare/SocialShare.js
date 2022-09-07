a29014a837acd6da7f6531fd375655598c6c9455
georgezikos <george.zikos@icloud.com>
2022-09-07 00:12:53 -0400

    Re-factoring  OG meta selector

--- src/components/SocialShare/SocialShare.js
+++ src/components/SocialShare/SocialShare.js
@@ -1,8 +1,8 @@
 // Global
 // const $body = $('body');
 const $window = $(window);
-const $metaTitle = $('meta[property=og:title]');
-const $metaDesc = $('meta[property=og:description]');
+const $metaTitle = $("meta[property='og:title']");
+const $metaDesc = $("meta[property='og:description']");
 
 // URL + Metadata
 const $currentPage = $(location).attr('href'); // for copying and emails
