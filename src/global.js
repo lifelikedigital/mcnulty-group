313fbc5bc322532d9b6ad46af04d50745651a6f8
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-10 21:05:57 -0400

    Pushing to trigger Netlify branch deploy

--- src/global.js
+++ src/global.js
@@ -1,13 +1,13 @@
 import Returns from './components/Returns/Returns';
 import Questions from './components/Questions/Questions';
 import SocialShare from './components/SocialShare/SocialShare';
-import Copyright from './components/Copyright/Copyright';
+// import Copyright from './components/Copyright/Copyright';
 
 const GlobalComponents = () => {
   Returns();
   Questions();
   SocialShare();
-  Copyright();
+  // Copyright();
 };
 
 const init = () => {
