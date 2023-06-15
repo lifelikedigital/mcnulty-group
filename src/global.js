6902381436b59e7b7f08118668455f020aab1106
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-14 21:53:35 -0400

    Adding progress bar component

--- src/global.js
+++ src/global.js
@@ -2,12 +2,14 @@ import Returns from './components/Returns/Returns';
 import Questions from './components/Questions/Questions';
 import SocialShare from './components/SocialShare/SocialShare';
 import Copyright from './components/Copyright/Copyright';
+import ProgressBar from './components/ProgressBar/ProgressBar';
 
 const GlobalComponents = () => {
   Returns();
   Questions();
   SocialShare();
   Copyright();
+  ProgressBar();
 };
 
 const init = () => {
