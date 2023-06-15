f558160a2c62360260e7817d388d8eb216cb17c1
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-14 21:58:02 -0400

    Trying to implement progress bar component again

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -19,9 +19,30 @@ function addProgressBar() {
 const ProgressBar = () => {
   addProgressBar();
   // Codyhouse
+  // utility functions
+  if (!Util) function Util() {}
+
+  Util.addClass = function (el, className) {
+    var classList = className.split(' ');
+    el.classList.add(classList[0]);
+    if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+  };
+
+  Util.removeClass = function (el, className) {
+    var classList = className.split(' ');
+    el.classList.remove(classList[0]);
+    if (classList.length > 1)
+      Util.removeClass(el, classList.slice(1).join(' '));
+  };
+
+  Util.toggleClass = function (el, className, bool) {
+    if (bool) Util.addClass(el, className);
+    else Util.removeClass(el, className);
+  };
+
+  // File#: _1_reading-progressbar
+  // Usage: codyhouse.co/license
   (function () {
-    // File#: _1_reading-progressbar
-    // Usage: codyhouse.co/license
     var readingIndicator = document.getElementsByClassName(
         'js-reading-progressbar'
       )[0],
