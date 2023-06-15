b1b1a8f52ae3f17d7db035c63ee3ed00b7dbe685
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-14 23:29:09 -0400

    Just testing if that made it better or worse

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -80,7 +80,7 @@ const Questions = () => {
       $this.find($toggleIcon).addClass(activeToggleIcon);
       gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 0 }, { duration: 0.25, rotation: 90 });
     }
-    updateStickyParentHeight();
+    // updateStickyParentHeight();
   });
   // Enter handling
   $toggleButton.on('keyup', function (e) {
@@ -115,7 +115,7 @@ const Questions = () => {
         gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 0 }, { duration: 0.25, rotation: 90 });
       }
     }
-    updateStickyParentHeight();
+    // updateStickyParentHeight();
   });
   // Arrow handling
   $faqQuestionsParent.on('keydown', function (e) {
