184533efc79c3e300d85620f9da4e157ecae91f7
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-15 20:23:17 -0400

    Toying around with video modal component implementation

--- src/global.js
+++ src/global.js
@@ -2,12 +2,14 @@ import Questions from './components/Questions/Questions';
 import SocialShare from './components/SocialShare/SocialShare';
 import Copyright from './components/Copyright/Copyright';
 import ProgressBar from './components/ProgressBar/ProgressBar';
+import VideoModal from './components/VideoModal/VideoModal';
 
 const GlobalComponents = () => {
   Questions();
   SocialShare();
   Copyright();
   ProgressBar();
+  VideoModal();
 };
 
 const init = () => {
