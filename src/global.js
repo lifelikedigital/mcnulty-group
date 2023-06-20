3a8f810672e362f5da49139a00b6294e70639e4a
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-19 21:11:34 -0400

    Implementing sticky banner component

--- src/global.js
+++ src/global.js
@@ -3,6 +3,7 @@ import SocialShare from './components/SocialShare/SocialShare';
 import Copyright from './components/Copyright/Copyright';
 import ProgressBar from './components/ProgressBar/ProgressBar';
 import VideoModal from './components/VideoModal/VideoModal';
+import StickyBanner from './components/StickyBanner/StickyBanner';
 
 const GlobalComponents = () => {
   Questions();
@@ -10,6 +11,7 @@ const GlobalComponents = () => {
   Copyright();
   ProgressBar();
   VideoModal();
+  StickyBanner();
 };
 
 const init = () => {
