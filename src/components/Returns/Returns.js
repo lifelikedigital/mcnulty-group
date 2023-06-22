4a1c7b88fe0b24f3b82235390c8630352d58c3b2
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-22 14:29:42 -0400

    Trying a cheeky workaround for this bar animation

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -8,6 +8,7 @@ const Returns = () => {
           tdElements.forEach((td) => {
             td.style.animation =
               'revealing-bars 4s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards'; // easeOutQuad
+            td.style.transform = 'scaleY(1)'; // Add this line
           });
           observer.disconnect();
         }
