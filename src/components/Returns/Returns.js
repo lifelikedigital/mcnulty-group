34fffb8d5eee002692b391b093a590c292219ba4
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-22 14:06:46 -0400

    Figuring it out

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -8,6 +8,7 @@ const Returns = () => {
           tdElements.forEach((td) => {
             td.style.animation =
               'revealing-bars 4s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards';
+            td.style.animationFillMode = 'forwards';
           });
           observer.disconnect();
         }
