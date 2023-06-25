3df9d36a035ddea2598d4b77ee6e3ea3a1a3d106
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-24 22:52:01 -0400

    Fine-tuning threshold for one element

--- src/components/FadeIn/FadeIn.js
+++ src/components/FadeIn/FadeIn.js
@@ -10,16 +10,29 @@ const FadeIn = () => {
   };
 
   const fadeInSection = document.querySelectorAll('.fade-in'); // Select elements with the fade-in class
+  const introIllustration = document.querySelector(
+    '.introduction__illustration'
+  ); // Select the specific element
 
   const observerOptions = {
-    threshold: 0.5, // Trigger the callback when 90% of the element is in the viewport
+    threshold: 0.5, // Trigger the callback when 50% of the element is in the viewport
+  };
+
+  const introIllustrationObserverOptions = {
+    threshold: 0.1, // Trigger the callback when 10% of the element is in the viewport
   };
 
   const observer = new IntersectionObserver(observerCallback, observerOptions);
+  const introIllustrationObserver = new IntersectionObserver(
+    observerCallback,
+    introIllustrationObserverOptions
+  );
 
   fadeInSection.forEach((element) => {
     observer.observe(element); // Observe each element with the fade-in class
   });
+
+  introIllustrationObserver.observe(introIllustration); // Observe the specific element with the custom threshold
 };
 
 export default FadeIn;
