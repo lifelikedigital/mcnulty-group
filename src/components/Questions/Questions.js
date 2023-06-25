ce191f0d5dae7e7b3cb735983f792bfc25f94aaa
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-24 20:56:28 -0400

    Custom easing on FAQ toggle icons

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -108,10 +108,10 @@ const Questions = () => {
     // prettier-ignore
     if ($this.find($toggleIcon).hasClass(activeToggleIcon)) {
       $this.find($toggleIcon).removeClass(activeToggleIcon);
-      gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 180 }, { duration: 0.2, rotation: 90 });
+      gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 180 }, { duration: 0.2, rotation: 90, ease: CustomEase.create("custom", "M0,0 C0.17,0.67 0.83,0.67 1,1 ") });
     } else {
       $this.find($toggleIcon).addClass(activeToggleIcon);
-      gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 90 }, { duration: 0.2, rotation: 180 });
+      gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 90 }, { duration: 0.2, rotation: 180, ease: CustomEase.create("custom", "M0,0 C0.17,0.67 0.83,0.67 1,1 ") });
     }
     if (window.innerWidth >= 768) {
       updateStickyParentHeight();
@@ -150,10 +150,10 @@ const Questions = () => {
       // prettier-ignore
       if ($this.find($toggleIcon).hasClass(activeToggleIcon)) {
         $this.find($toggleIcon).removeClass(activeToggleIcon);
-        gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 180 }, { duration: 0.2, rotation: 90 });
+        gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 180 }, { duration: 0.2, rotation: 90, ease: CustomEase.create("custom", "M0,0 C0.17,0.67 0.83,0.67 1,1 ") });
       } else {
         $this.find($toggleIcon).addClass(activeToggleIcon);
-        gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 90 }, { duration: 0.2, rotation: 180 });
+        gsap.fromTo($this.find($toggleIcon), { duration: 0.2, rotation: 90 }, { duration: 0.2, rotation: 180, ease: CustomEase.create("custom", "M0,0 C0.17,0.67 0.83,0.67 1,1 ") });
       }
       if (window.innerWidth >= 768) {
         updateStickyParentHeight();
