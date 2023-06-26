dff0ab28410b04e16110672f839e3af69772046d
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-26 11:44:25 -0400

    Adding breakpoints to chart animation script

--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -1,5 +1,4 @@
 const Returns = () => {
-  // Use the Intersection Observer API to detect when 80% of the target element is in the viewport
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
@@ -8,7 +7,11 @@ const Returns = () => {
           tdElements.forEach((td) => {
             td.style.animation =
               'revealing-bars 4s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards'; // easeOutQuad
-            td.style.transform = 'scale3d(1, 1, 1) translateY(1.35px)';
+            if (window.innerWidth <= 991) {
+              td.style.transform = 'scale3d(1, 1, 1) translateY(0.95px)';
+            } else {
+              td.style.transform = 'scale3d(1, 1, 1) translateY(1.35px)';
+            }
           });
           observer.disconnect();
         }
@@ -17,10 +20,7 @@ const Returns = () => {
     { threshold: 0.9 }
   );
 
-  // Select the target element
   const targetElement = document.querySelector('#returns');
-
-  // Observe the target element
   observer.observe(targetElement);
 };
 
