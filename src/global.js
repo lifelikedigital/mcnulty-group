c337fe6eb10b3cf8ac055a6a6cbf0d88466516a4
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-06-26 21:03:01 -0400

    OOPS

--- src/global.js
+++ src/global.js
@@ -1,7 +1,6 @@
 import FadeIn from './components/FadeIn/FadeIn';
 import Questions from './components/Questions/Questions';
 import SocialShare from './components/SocialShare/SocialShare';
-import Copyright from './components/Copyright/Copyright';
 import ProgressBar from './components/ProgressBar/ProgressBar';
 import VideoModal from './components/VideoModal/VideoModal';
 import StickyBanner from './components/StickyBanner/StickyBanner';
