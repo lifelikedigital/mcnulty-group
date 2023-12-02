39af496db5d5306bc42ab3e98c9b6f22534c42e9
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-02 17:34:33 -0500

    Stripping away unneeded code from previous build

--- src/global.js
+++ src/global.js
@@ -1,19 +1,13 @@
-import FadeIn from './components/FadeIn/FadeIn';
+// import FadeIn from './components/FadeIn/FadeIn';
 import Questions from './components/Questions/Questions';
-import ProgressBar from './components/ProgressBar/ProgressBar';
-import VideoModal from './components/VideoModal/VideoModal';
-import StickyBanner from './components/StickyBanner/StickyBanner';
-import Returns from './components/Returns/Returns';
-import Copyright from './components/Copyright/Copyright';
+// import VideoModal from './components/VideoModal/VideoModal';
+// import Copyright from './components/Copyright/Copyright';
 
 const GlobalComponents = () => {
-  FadeIn();
-  ProgressBar();
-  StickyBanner();
-  VideoModal();
-  Returns();
+  // FadeIn();
   Questions();
-  Copyright();
+  // VideoModal();
+  // Copyright();
 };
 
 const init = () => {
