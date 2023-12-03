41284819f9b299566de72927d9c8faecce1c5602
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-02 21:10:23 -0500

    Same

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -37,7 +37,7 @@ const Questions = () => {
     $otherActiveToggleIcon.removeClass(activeToggleIcon);
     gsap.to($otherActiveToggleIcon, {
       duration: 0.3,
-      rotation: 90,
+      rotation: 0,
       ease: 'basicEase',
     });
     const $thisToggleIcon = $this.find($toggleIcon);
@@ -99,7 +99,7 @@ const Questions = () => {
       $otherActiveToggleIcon.removeClass(activeToggleIcon);
       gsap.to($otherActiveToggleIcon, {
         duration: 0.3,
-        rotation: 90,
+        rotation: 0,
         ease: 'basicEase',
       });
       const $thisToggleIcon = $this.find($toggleIcon);
