fa3eafeecb10ffa404c683bf736480b5cae4bac9
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-02 21:33:01 -0500

    More

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -87,9 +87,9 @@ const Questions = () => {
     const enterKey = 13;
     if (e.keyCode === enterKey) {
       const $this = $(this);
-      // console.log($this);
+      console.log($this);
       const $otherTabs = $this.parent().siblings().find($toggleButton);
-      // console.log($otherTabs);
+      console.log($otherTabs);
       $otherTabs.removeClass(activeToggleButton).attr('aria-selected', 'false');
       $otherTabs
         .next()
