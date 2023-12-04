bf095da8f270c31037761d3836301195cacfc969
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-04 06:26:44 -0500

    Previous and next buttons

--- src/components/Carousels/YourTeamCarousel.js
+++ src/components/Carousels/YourTeamCarousel.js
@@ -8,6 +8,21 @@ const YourTeamCarousel = () => {
   };
   const carousels = CreateCarousel(selector, options);
 
+  // Assuming the carousel has an ID of 'service-highlights-carousel'
+  const carouselId = 'service-highlights-carousel';
+
+  // Select the buttons using the carousel ID
+  const prevButton = document.querySelector(
+    `#${carouselId} #service-highlights-prev`
+  );
+  const nextButton = document.querySelector(
+    `#${carouselId} #service-highlights-next`
+  );
+
+  // Add event listeners to the buttons
+  prevButton.addEventListener('click', () => carousels[0].scrollPrev());
+  nextButton.addEventListener('click', () => carousels[0].scrollNext());
+
   // Add any additional behavior here
 };
 
