6a8c9f4836e404df71824a373e200447f515eba3
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-04 04:18:42 -0500

    Additional interactions

--- src/components/SpendingProfile/SpendingProfile.js
+++ src/components/SpendingProfile/SpendingProfile.js
@@ -1,9 +1,13 @@
 const actionElement = document.getElementById('spending-profile-core-action');
 const cardElement = document.getElementById('spending-profile-core-card');
+const hideElement = document.getElementById('hide-core-spending-profile');
+const showElement = document.getElementById('see-core-spending-profile');
 
 const SpendingProfile = () => {
   actionElement.addEventListener('click', () => {
     cardElement.classList.toggle('spending-profile--active');
+    hideElement.classList.toggle('hide');
+    showElement.classList.toggle('show');
   });
 };
 
