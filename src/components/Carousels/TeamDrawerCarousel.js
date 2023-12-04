f377e9901f8dd00bf25f071108069f35dcd76cee
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-04 08:58:44 -0500

    Experimenting

--- src/components/Carousels/TeamDrawerCarousel.js
+++ src/components/Carousels/TeamDrawerCarousel.js
@@ -1,15 +1,19 @@
-import CreateCarousel from './CreateCarousel';
+import { createEmblaCarousel } from 'embla-carousel';
+import { createEmblaCarouselAutoHeight } from 'embla-carousel-auto-height';
 
 const TeamDrawerCarousel = () => {
-  const selector = '#team-drawer-carousel .embla__viewport';
+  const emblaNode = document.querySelector(
+    '#team-drawer-carousel .embla__viewport'
+  );
   const options = {
     loop: false,
     align: 'start',
   };
-  const carousels = CreateCarousel(selector, options);
-  const carousel = carousels[0];
 
-  // Assuming the carousel has an ID of 'service-highlights-carousel'
+  const embla = createEmblaCarousel(emblaNode, options);
+  const autoHeight = createEmblaCarouselAutoHeight(embla);
+
+  // Assuming the carousel has an ID of 'team-drawer-carousel'
   const carouselId = 'team-drawer-carousel';
 
   // Select the buttons using the carousel ID
@@ -19,28 +23,31 @@ const TeamDrawerCarousel = () => {
   // Function to update button styles
   const updateButtonStyles = () => {
     if (prevButton && nextButton) {
-      prevButton.classList.toggle('is-disabled', !carousel.canScrollPrev());
-      nextButton.classList.toggle('is-disabled', !carousel.canScrollNext());
+      prevButton.classList.toggle('is-disabled', !embla.canScrollPrev());
+      nextButton.classList.toggle('is-disabled', !embla.canScrollNext());
     }
   };
 
   // Add event listeners to the buttons
   if (prevButton && nextButton) {
     prevButton.addEventListener('click', () => {
-      carousel.scrollPrev();
+      embla.scrollPrev();
       updateButtonStyles();
     });
     nextButton.addEventListener('click', () => {
-      carousel.scrollNext();
+      embla.scrollNext();
       updateButtonStyles();
     });
   }
 
   // Update button styles when the carousel's scroll position changes
-  carousel.on('scroll', updateButtonStyles);
+  embla.on('scroll', updateButtonStyles);
 
   // Update button styles initially
   updateButtonStyles();
+
+  // Initialize the Auto Height plugin
+  autoHeight.update();
 
   // Add any additional behavior here
 };
