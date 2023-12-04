490fb65fd885f90ce208e4d695b22240a6168975
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-04 06:12:51 -0500

    Embla fine-tuning

--- src/components/Carousels/YourTeamCarousel.js
+++ src/components/Carousels/YourTeamCarousel.js
@@ -2,7 +2,10 @@ import CreateCarousel from './CreateCarousel';
 
 const YourTeamCarousel = () => {
   const selector = '#service-highlights-carousel .embla__viewport';
-  const options = { loop: false }; // customize as needed
+  const options = {
+    loop: false,
+    containScroll: 'trimSnaps', // Add this line
+  };
   const carousels = CreateCarousel(selector, options);
 
   // Add any additional behavior here
