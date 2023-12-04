4107a40c6c817248f7bd55fcf4469d8101928843
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-04 04:21:20 -0500

    Right idea, wrong elements

--- src/components/SpendingProfile/SpendingProfile.js
+++ src/components/SpendingProfile/SpendingProfile.js
@@ -1,7 +1,7 @@
 const actionElement = document.getElementById('spending-profile-core-action');
 const cardElement = document.getElementById('spending-profile-core-card');
-const hideElement = document.getElementById('hide-core-spending-profile');
-const showElement = document.getElementById('see-core-spending-profile');
+const hideElement = document.getElementById('see-core-spending-profile');
+const showElement = document.getElementById('hide-core-spending-profile');
 
 const SpendingProfile = () => {
   actionElement.addEventListener('click', () => {
