2fa5d828e52204261f1907c06bd1c7e17496e2b3
georgezikos <53711433+georgezikos@users.noreply.github.com>
2023-12-04 09:31:33 -0500

    Making sure CreateCarousel works

--- src/components/Carousels/CreateCarousel.js
+++ src/components/Carousels/CreateCarousel.js
@@ -1,11 +1,21 @@
 import EmblaCarousel from 'embla-carousel';
+import { createEmblaCarouselAutoHeight } from 'embla-carousel-auto-height';
 
-export default function CreateCarousel(selector, options = {}) {
+export default function CreateCarousel(
+  selector,
+  options = {},
+  autoHeight = false
+) {
   const carouselNodes = document.querySelectorAll(selector);
   const carousels = [];
 
   carouselNodes.forEach((node) => {
-    const carousel = EmblaCarousel(node, options);
+    const plugins = [];
+    if (autoHeight) {
+      const autoHeightOptions = { destroyHeight: 'auto' }; // Options for AutoHeight plugin
+      plugins.push(createEmblaCarouselAutoHeight(autoHeightOptions)); // Add AutoHeight plugin with options
+    }
+    const carousel = EmblaCarousel(node, options, plugins);
     carousels.push(carousel);
   });
 
