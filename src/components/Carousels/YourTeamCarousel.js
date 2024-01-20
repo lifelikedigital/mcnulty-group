d5b0c2d519126ab83fb4652d34af17832ee37e07
georgezikos <53711433+georgezikos@users.noreply.github.com>
2024-01-19 20:00:55 -0500

    Switching embla align setting back

--- src/components/Carousels/YourTeamCarousel.js
+++ src/components/Carousels/YourTeamCarousel.js
@@ -4,7 +4,7 @@ const YourTeamCarousel = () => {
   const selector = '#service-highlights-carousel .embla__viewport';
   const options = {
     loop: false,
-    align: 'center',
+    align: 'start',
   };
   const carousels = CreateCarousel(selector, options);
   const carousel = carousels[0];
