6fb546ae34182a6a185d05cfff86bc5cba5d05dc
georgezikos <53711433+georgezikos@users.noreply.github.com>
2024-01-23 18:41:00 -0500

    Had to revise the FadeIn component

--- src/components/FadeIn/FadeIn.js
+++ src/components/FadeIn/FadeIn.js
@@ -9,23 +9,13 @@ const FadeIn = () => {
   };
 
   const fadeInSection = document.querySelectorAll('.fade-in'); // Select elements with the fade-in class
-  const introIllustration = document.querySelector(
-    '.introduction__illustration'
-  ); // Select the specific element
 
   const observerOptions = {
     threshold: 0.5, // Trigger the callback when 50% of the element is in the viewport
   };
 
-  const introIllustrationObserverOptions = {
-    threshold: 0.1, // Trigger the callback when 10% of the element is in the viewport
-  };
-
   const observer = new IntersectionObserver(observerCallback, observerOptions);
-  const introIllustrationObserver = new IntersectionObserver(
-    observerCallback,
-    introIllustrationObserverOptions
-  );
+  const introIllustrationObserver = new IntersectionObserver(observerCallback);
 
   fadeInSection.forEach((element) => {
     observer.observe(element); // Observe each element with the fade-in class
